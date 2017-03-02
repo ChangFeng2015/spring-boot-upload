db696168e1da2a4f7e144b435207049cfc0f23f7
wangdwe <wangdwe@yonyou.com>
2017-03-02 11:18:08 +0800

    使用GetMapping代替RequestMapping

--- src/main/java/com/springboot/controller/HelloController.java
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -1,9 +1,8 @@
 package com.springboot.controller;
 
+import com.springboot.domain.Demo;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Dave on 2017/3/1.
@@ -18,9 +17,23 @@ public class HelloController
     @Value("${cup.content}")
     private String content;
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+//    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @GetMapping(value = "/hello")
     public String hello()
     {
         return content;
     }
+
+    @PostMapping(value = "/hello")
+    public Demo getDemo(@RequestParam(value = "id",required = false,defaultValue = "0")long id,
+                        @RequestParam(value = "userName",required = false,defaultValue = "Tom")String userName,
+                        @RequestParam(value = "age",required = false,defaultValue = "18")int age)
+    {
+        Demo demo = new Demo();
+        demo.setId(id);
+        demo.setUserName(userName);
+        demo.setAge(age);
+        return demo;
+    }
+
 }
