ca7761958ce173b9da9ebcf90d9fa5075248d20f
Dave <kingdavehangzhou@gmail.com>
2018-04-11 18:49:04 +0800

    aop test

---
+++ src/main/java/com/baitu/shop/annotation/Loggable.java
@@ -0,0 +1,17 @@
+package com.baitu.shop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Loggable {
+
+    public String optType();
+
+    public String describe();
+
+    public String module();
+}
---
+++ src/main/java/com/baitu/shop/aop/WebLogAspect.java
@@ -0,0 +1,45 @@
+package com.baitu.shop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Order(5)
+public class WebLogAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebLogAspect.class);
+
+    @Pointcut("execution(public * com.baitu.shop.controller..*(..))")
+    public void webLog() {
+
+    }
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        LOGGER.info("URL ： " + request.getRequestURL().toString());
+        LOGGER.info("HTTP_METHOD : " + request.getMethod());
+        LOGGER.info("IP : " + request.getRemoteAddr());
+        LOGGER.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        LOGGER.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        LOGGER.info("response : " + ret);
+    }
+}
