f1cf5150c49a3601dcb3800a399802f89870044c
Dave <kingdavehangzhou@gmail.com>
2018-06-07 00:08:47 +0800

    file upload

--- src/main/java/com/baitu/shop/ShopApplication.java
+++ src/main/java/com/baitu/shop/ShopApplication.java
@@ -2,13 +2,12 @@ package com.baitu.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Dave on 2017/5/24.
  */
 @SpringBootApplication
-public class ShopApplication extends WebMvcConfigurerAdapter {
+public class ShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
     }
---
+++ src/main/java/com/baitu/shop/controller/RestUploadController.java
@@ -0,0 +1,101 @@
+package com.baitu.shop.controller;
+
+import com.baitu.shop.domain.UploadModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class RestUploadController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(RestUploadController.class);
+
+    private static String UPLOADED_FOLDER = "D://temp//";
+
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile) {
+        if (uploadfile.isEmpty()) {
+            return new ResponseEntity("Please select a file", HttpStatus.OK);
+        }
+        try {
+            saveUploadedFiles(Arrays.asList(uploadfile));
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Success uploaded - " + uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
+    }
+
+    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
+        for (MultipartFile file : files) {
+            if (file.isEmpty()) {
+                continue;
+            }
+
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+        }
+    }
+
+    // Multiple file upload
+    @PostMapping("/api/upload/multi")
+    public ResponseEntity<?> uploadFileMulti(
+            @RequestParam("extraField") String extraField,
+            @RequestParam("files") MultipartFile[] uploadfiles) {
+
+        LOGGER.debug("Multiple file upload!");
+
+        String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
+                .filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
+
+        if (StringUtils.isEmpty(uploadedFileName)) {
+            return new ResponseEntity("please select a file!", HttpStatus.OK);
+        }
+
+        try {
+
+            saveUploadedFiles(Arrays.asList(uploadfiles));
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("Successfully uploaded - "
+                + uploadedFileName, HttpStatus.OK);
+
+    }
+
+    // maps html form to a Model
+    @PostMapping("/api/upload/multi/model")
+    public ResponseEntity<?> multiUploadFileModel(@ModelAttribute UploadModel model) {
+
+        LOGGER.debug("Multiple file upload! With UploadModel");
+
+        try {
+
+            saveUploadedFiles(Arrays.asList(model.getFiles()));
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("Successfully uploaded!", HttpStatus.OK);
+
+    }
+
+}
